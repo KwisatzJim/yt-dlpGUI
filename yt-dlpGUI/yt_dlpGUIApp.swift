24fae958a3a43bebb33c4179aec76166df2d50a8
Jim Kelley <131250+KwisatzJim@users.noreply.github.com>
2025-04-16 20:53:15 -0500

    Initial Commit

---
+++ yt-dlpGUI/yt_dlpGUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  yt_dlpGUIApp.swift
+//  yt-dlpGUI
+//
+//  Created by Jim Kelley on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct yt_dlpGUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ yt-dlpGUITests/yt_dlpGUITests.swift
@@ -0,0 +1,17 @@
+//
+//  yt_dlpGUITests.swift
+//  yt-dlpGUITests
+//
+//  Created by Jim Kelley on 4/16/25.
+//
+
+import Testing
+@testable import yt_dlpGUI
+
+struct yt_dlpGUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
